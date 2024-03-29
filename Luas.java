a6da891bcea70c3eb900744c0579c213dc636992
muhammadaqil12 <115065268+muhammadaqil12@users.noreply.github.com>
2024-03-29 18:28:21 +0700

    Create Luas.java

---
+++ Luas.java
@@ -0,0 +1,17 @@
+ public class Luas {
+    public static void main(String[] args) {
+        //Input
+        Double tinggi = 15.00;
+        Double a = 10.00;
+        Double b = 8.00;
+        Double c = 6.00;
+
+        //Process
+        Double luas = 1.00/2 * a * tinggi;
+        Double keliling = a + b + c;
+
+        //Output
+        System.out.println("luas segitiga: " + luas);
+        System.out.println("Keliling segitiga: " + keliling);
+    }
+}
