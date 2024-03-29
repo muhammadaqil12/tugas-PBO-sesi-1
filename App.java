96ad07c00afaa5927f62c5d8899f06cbb1ccc1b9
muhammadaqil12 <115065268+muhammadaqil12@users.noreply.github.com>
2024-03-29 18:27:51 +0700

    Create App.java

---
+++ App.java
@@ -0,0 +1,33 @@
+public class App {
+    public static void main(String[] args) {
+        //Input
+        Double reamur = 80.00;
+
+        //Process
+        Double celcius = 5.00/4 * reamur;
+        Double fahrenheit = 9.00/5 * celcius + 32;
+        Double kelvin = celcius * 273;
+
+        //Output
+        System.out.println("Reamur: " + reamur);
+        System.out.println("Celcius: " + celcius);
+        System.out.println("Fahrenheit: " + fahrenheit);
+        System.out.println("Kelvin: " + kelvin);
+    }
+}public class App {
+    public static void main(String[] args) {
+        //Input
+        Double reamur = 80.00;
+
+        //Process
+        Double celcius = 5.00/4 * reamur;
+        Double fahrenheit = 9.00/5 * celcius + 32;
+        Double kelvin = celcius * 273;
+
+        //Output
+        System.out.println("Reamur: " + reamur);
+        System.out.println("Celcius: " + celcius);
+        System.out.println("Fahrenheit: " + fahrenheit);
+        System.out.println("Kelvin: " + kelvin);
+    }
+}
