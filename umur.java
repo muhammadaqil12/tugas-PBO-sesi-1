4f878860db3e2fcf4597da550eea996fb1ef0f7f
muhammadaqil12 <115065268+muhammadaqil12@users.noreply.github.com>
2024-03-29 18:28:42 +0700

    Create umur.java

---
+++ umur.java
@@ -0,0 +1,13 @@
+public class Umur {
+    public static void main(String[] args) {
+       //Input   
+       int tahunLahir = 2004;
+       int tahunSekarang = 2024;
+
+       //Process
+       int umur = tahunSekarang - tahunLahir;
+
+       //Output
+       System.out.println("Umur Aku sekarang" + umur + "tahun");
+    }
+}
